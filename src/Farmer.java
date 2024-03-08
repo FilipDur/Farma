5b54de44a0f4137b5afc0de58a68564f97288735
Filko <filiposduricic24@gmail.com>
2024-03-08 13:26:18 +0100

    pridani moznosti zasadit rostlinu treba jako 🍆(lilek)

---
+++ src/Farmer.java
@@ -0,0 +1,10 @@
+public class Farmer extends Zahonek {
+    private String name;
+    private int age;
+    private int height;
+    private int weight;
+public void plant(String kyticka){
+    farma.add(kyticka);
+}
+
+}
---
+++ src/Stodola.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Stodola {
+    private String name;
+    ArrayList<Animal> zvirata=new ArrayList<>();
+
+    public void addAnimal(Animal zvirata){
+
+    }
+}
