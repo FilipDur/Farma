ab3b97a64d171f15495438d5835c5426f4096b8b
Filko <filiposduricic24@gmail.com>
2024-03-08 13:37:43 +0100

    pridal jsem tridu Stodola do ktere jsem dal metodu na pridani zvirat do stodola

--- src/Stodola.java
+++ src/Stodola.java
@@ -2,9 +2,10 @@ import java.util.ArrayList;
 
 public class Stodola {
     private String name;
-    ArrayList<Animal> zvirata=new ArrayList<>();
+    ArrayList<Animal> stodulka=new ArrayList<>();
 
     public void addAnimal(Animal zvirata){
-
+        addAnimal(zvirata);
     }
+
 }
